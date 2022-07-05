4d9867db2b6b8fad096d80bb22f98a5590f5cd52
Vitor Shigueta <vshigueta@gmail.com>
2022-07-05 16:12:49 -0300

    Primeiro commit

---
+++ src/Exercicio1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String frase = sc.nextLine();
+        System.out.println(frase);
+    }
+}
---
+++ src/Exercicio3.java
@@ -0,0 +1,7 @@
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello 3");
+    }
+
+}
